fbcf33288250ab60a731a833833e02fc64997b1a
sthefany <sthefanymendozanarvaez@gmail.com>
2020-06-15 18:51:35 -0500

    ver3

--- Contactanos.php
+++ Contactanos.php
@@ -131,21 +131,7 @@ font-size: 20px;
 
 
 </style>
-  <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div class="container">
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        
+  
 
     
 
