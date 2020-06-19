808915b19b4e0327010f2e13e56506fa5a9031f3
sthefany <sthefanymendozanarvaez@gmail.com>
2020-06-19 01:29:22 -0500

    actualizacion

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-include "header.php";
+include "header.php"
 ?>
   
 
@@ -40,38 +40,7 @@ include "header.php";
       </div>
     </section>
 
-   
-      <!-- pie de pagina -->
-    <footer>
-		<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-			<div class="container">
-			  <button
-				class="navbar-toggler"
-				type="button"
-				data-toggle="collapse"
-				data-target="#navbarNav"
-				aria-controls="navbarNav"
-				aria-expanded="false"
-				aria-label="Toggle navigation"
-			  >
-				<span class="navbar-toggler-icon"></span>
-			  </button>
-			  <div class="collapse navbar-collapse" id="navbarNav">
-				<ul class="navbar-nav ml-auto">
-				  <li class="nav-item">
-					<a class="nav-link" href="#">Nosotros</a>
-				  </li>
-				  <li class="nav-item">
-					<a class="nav-link" href="#">Servicios</a>
-				  </li>
-				  <li class="nav-item">
-					<a class="nav-link" href="#">Contactanos</a>
-				  </li>
-				</ul>
-			  </div>
-			</div>
-		  </nav>
-    </footer>
+  
 
 
 
