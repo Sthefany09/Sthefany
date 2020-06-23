fef59fc7a7330955d1a55f1831e4f1ea9e37ac6c
sthefany <sthefanymendozanarvaez@gmail.com>
2020-06-23 16:50:17 -0500

    cualquiercosa

--- Servicios.php
+++ Servicios.php
@@ -26,6 +26,29 @@ include "header.php";
       </div>
     </section>
 
+
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="..." alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="..." alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="..." alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
     <section class="sect2">
       <article class="buzo">
         <img  src="Imagenes/dd.jpg" alt="">
