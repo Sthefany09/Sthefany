c928267e247a93288282997c05e25e398008a450
sthefany <sthefanymendozanarvaez@gmail.com>
2020-06-22 23:27:28 -0500

    actualizacion

--- header.php
+++ header.php
@@ -149,6 +149,41 @@ font-size: 20px;
 
 
 
+    <!-- NEWSLETTER  -->
+    <section id="newsletter" class="bg-dark text-white py-5">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <h1>SERVICIOS</h1>
+            <br /><br />
+            <p>
+              A continuación te mostraremos nuestro catalogo, las promociones y
+              los servicios en general que podemos ofrecerte. <br> <br> 
+              <h2 class="subtitulos">Vestidos.</h2>  <br><br> 
+              <h2 class="subtitulos">Blusas y pantalones.</h2> <br><br>
+              <h2 class="subtitulos">Zapatos. Chaquetas.</h2>
+            </p>
+
+            
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="sect2">
+      <article class="buzo">
+        <img  src="Imagenes/dd.jpg" alt="">
+        <img src="Imagenes/negro.jpg" alt="">
+        <img src="Imagenes/vestidonegro2.jpg" alt="">
+        <img src="Imagenes/vestidonegro.jpg" alt="">
+      </article>
+    
+      
+    </section>
+
+  
+
+
     <!-- BOOTSTRAP SCRIPTS -->
     <script
       src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
@@ -169,6 +204,3 @@ font-size: 20px;
 </html>
 
 
-<?php 
-include "footer.php";
-?>
