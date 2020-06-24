9049c364a60f77ba7e4fd629097924af3af6dcaa
sthefany <sthefanymendozanarvaez@gmail.com>
2020-06-23 20:46:26 -0500

    actualizacion

--- Servicios.php
+++ Servicios.php
@@ -1,9 +1,5 @@
-<?php 
-include "header.php";
-?>
-    
-         
-    
+
+          
 
     <!-- NEWSLETTER  -->
     <section id="newsletter" class="bg-dark text-white py-5">
--- header.php
+++ header.php
@@ -179,10 +179,7 @@ font-size: 20px;
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="sr-only">Previous</span>
   </a>
-  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
+  
 </div>
 
     
