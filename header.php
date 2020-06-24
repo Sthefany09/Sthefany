571675cf7c074e7642df18c17b6ea680b75d2c9e
sthefany <sthefanymendozanarvaez@gmail.com>
2020-06-23 19:19:17 -0500

    actualizacion

--- header.php
+++ header.php
@@ -80,6 +80,27 @@ font-size: 20px;
       </div>
     </header>
 
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-50" src="Imagenes/rock.jpg" alt="First slide">
+    </div>
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="Imagenes/collaje.jpg" alt="First slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+
+
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="container">
         <button
