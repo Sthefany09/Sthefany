dbdc6a27552c6a28e24f68d599e5757cc2f12196
sthefany <sthefanymendozanarvaez@gmail.com>
2020-06-23 20:09:40 -0500

    actualizacion

--- header.php
+++ header.php
@@ -152,25 +152,7 @@ font-size: 20px;
 
   
    
- <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="d-block w-70" src="Imagenes/blu.jpg" alt="First slide">
-    </div>
-    <div class="carousel-item active">
-      <img class="d-block w-70" src="Imagenes/rock.jpg" alt="First slide">
-    </div>
-    
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
+
 
     
 
@@ -194,6 +176,27 @@ font-size: 20px;
     ></script>
   </body>
 </html>
+
+
+ <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="Imagenes/blu.jpg" alt="First slide">
+    </div>
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="Imagenes/rock.jpg" alt="First slide">
+    </div>
+    
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
 
 
 
